6d1d7b2ac841748cd787223b0fcbefc33875e893
PranavPatil05 <pranavpatil962@gmail.com>
2023-10-15 16:55:59 +0530

    add redux toolkit

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react'
-import { Link,Navigate,useNavigate } from 'react-router-dom'
-
+import { Link,useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux';
+import { signInStart, signInSuccess,signInFailure } from '../redux/user/userSlice.js';
 
 export default function SignIn() {
   const [formData,setFormData] = useState({});
-  const [error,setError] = useState(null);
-  const [loading,setLoading] = useState(false);
+  const {loading,error} =useSelector((state)=>state.user);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const handleChange = (e) =>{
        setFormData(
         {
@@ -18,7 +19,7 @@ export default function SignIn() {
     e.preventDefault();
     try {
 
-      setLoading(true);
+     dispatch(signInStart());
       const res = await fetch('/api/auth/sign-in',
       {
         method:'POST',
@@ -29,16 +30,13 @@ export default function SignIn() {
       });
       const data = await res.json();
       if(data.success === false){
-        setLoading(false);
-        setError(data.message);
+        dispatch(signInFailure(data.message));
         return;
       }
-      setLoading(false);
-      setError(null);
+      dispatch(signInSuccess(data));
       navigate('/');
     } catch (error){
- setLoading(false);
- setError(error.message);
+      dispatch(signInFailure(data.message));
     }
   }
 
@@ -47,7 +45,7 @@ export default function SignIn() {
   return (
     
     <div className='p-3 max-w-lg mx-auto'>
-      <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
       <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
         <input type="email" placeholder='email'
         className='border p-3 rounded-lg' id='email' onChange={handleChange}/>
